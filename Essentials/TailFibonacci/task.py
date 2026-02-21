99beebbdf040723492237e80012ba2c21b4374f2
strateg17 <69148321+strateg17@users.noreply.github.com>
2026-02-21 16:37:50 +0200

    Update TailFibonacci task.py

--- Essentials/TailFibonacci/task.py
+++ Essentials/TailFibonacci/task.py
@@ -4,4 +4,14 @@ from Essentials.TailFibonacci.tail_recursion import tail_call_optimized
 
 
 def fibonacci_impl() -> Callable[[int], int]:
-    raise Exception('Implement me')
+
+    @tail_call_optimized
+    def fib_step(n: int, a: int, b: int) -> int:
+        if n == 0:
+            return a
+        return fib_step(n - 1, b, a + b)
+
+    def fibonacci(n: int) -> int:
+        return fib_step(n, 0, 1)
+
+    return fibonacci
