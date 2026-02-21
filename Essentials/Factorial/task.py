374555801d9f4af9446462163151194234403373
strateg17 <69148321+strateg17@users.noreply.github.com>
2026-02-21 16:03:51 +0200

    Factorial task.py

--- Essentials/Factorial/task.py
+++ Essentials/Factorial/task.py
@@ -4,4 +4,13 @@ from Essentials.Factorial.tail_recursion import tail_call_optimized
 
 
 def factorial_impl() -> Callable[[int], int]:
-    raise Exception('Implement me')
+    @tail_call_optimized
+    def fact(n: int, acc: int) -> int:
+        if n <= 1:
+            return acc
+        return fact(n - 1, n * acc)
+
+    def factorial(n: int) -> int:
+        return fact(n, ___)
+
+    return factorial
