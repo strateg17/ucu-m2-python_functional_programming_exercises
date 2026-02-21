6a3c46022451890e41d76a925b23561d2090be7d
strateg17 <69148321+strateg17@users.noreply.github.com>
2026-02-21 16:36:25 +0200

    Update Fibonnaci task.py

--- Essentials/Fibonacci/task.py
+++ Essentials/Fibonacci/task.py
@@ -2,4 +2,11 @@ from typing import Callable
 
 
 def fibonacci_impl() -> Callable[[int], int]:
-    raise Exception("Implement me")
+    def fibonacci(n: int) -> int:
+        if n < 0:
+            raise ValueError('fibonacci is undefined for negative numbers')
+        if n <= 1:
+            return n
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+    return fibonacci
